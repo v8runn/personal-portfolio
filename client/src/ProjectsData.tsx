701de71690f831ebdb3f25482848cd08fdc92436
Varun Shankar <varunshankar02@gmail.com>
2021-09-03 11:45:39 +0100

    projectsdata file created

---
+++ client/src/ProjectsData.tsx
@@ -0,0 +1,44 @@
+import inarow from "./inarow.png"
+import snakegame from "./snakegame.png"
+import supermarket from "./supermarket.png"
+import chess from "./chess.png"
+
+
+
+
+interface projects {
+    name: string,
+    image: string,
+    description: string,
+    link: string
+}
+
+const ProjectsData: projects[] = [
+    {
+        name: "in a row",
+        image: inarow,
+        description: "A modern adaptation of the childhood classic 'Connect 4' made as a part of my first-year group project with my tutor group. I was in charge of the front-end and it was created on ReactJS.",
+        link: "https://github.com/v8runn/in-a-row"
+    },
+    {
+        name: "the snake game",
+        image: snakegame,
+        description: "A fun classic recreated using the Python Tkinter module.",
+        link: "https://google.com"
+    },
+    {
+        name: "supermarket billing system",
+        image: supermarket,
+        description: "A supermarket billing system created on C++ using the OOP methods.",
+        link: "https://github.com/v8runn/SupermarketBillingSystem"
+    },
+    {
+        name: "chess",
+        image: chess,
+        description: "THe game of chess created on Java.",
+        link: "https://github.com/v8runn/java-chess-game"
+    }
+
+]
+
+export default ProjectsData;
